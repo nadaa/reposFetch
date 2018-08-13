f39a59658e32d6ab66b0c980af9be34204134da1
nadaa <nada.saif7@gmail.com>
2018-08-13 11:21:42 +0300

    update test

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 
 module.exports={
   db: '',
-  gitToken:'717803bcdbd3d1d2f9a0224f3d5e83167514cb12'
+  gitToken:''
 }
--- test/serverSpec.js
+++ test/serverSpec.js
@@ -4,25 +4,25 @@ const data=require('../data.json');
 
 describe('test api methods',function(){
 	it('should pass if a bookmark is addedd successfully',function(){
-		api.addBookmark(data,2881789);
-		expect(2881789).to.equal(data[0].bookmark);
+		api.addBookmark(data,32726038);
+		expect(32726038).to.equal(data[0].bookmark);
 	})
 
 	it('should pass when all bookmarks are retrieved',function(){
-		api.addBookmark(data,2881789);
-		api.addBookmark(data,63476337);
-		api.addBookmark(data,27247448);
+		api.addBookmark(data,32726038);
+		api.addBookmark(data,6859964);
+		api.addBookmark(data,78732259);
 		let bookmarks=api.getAllBookmarks(data);
 		expect(3).to.equal(bookmarks.length);
 	})
 
 	it('should pass when a bookmark is removed',function(){
-		api.addBookmark(data,2881789);
-		api.addBookmark(data,63476337);
-		api.addBookmark(data,27247448);
-		api.removeBookmark(data,2881789);
-		api.removeBookmark(data,63476337);
-		api.removeBookmark(data,27247448);
+		api.addBookmark(data,32726038);
+		api.addBookmark(data,6859964);
+		api.addBookmark(data,78732259);
+		api.removeBookmark(data,32726038);
+		api.removeBookmark(data,6859964);
+		api.removeBookmark(data,78732259);
 		let bookmarks=api.getAllBookmarks(data);
 		expect(0).to.equal(bookmarks.length);
 	})
